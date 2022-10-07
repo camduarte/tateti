df4f1941bf32b7c320c45b7f6687952242c15e09
cduarte <duarte.camd@gmail.com>
2022-10-07 18:15:02 -0300

    Agrego listeners a los botones.

--- js/tateti.js
+++ js/tateti.js
@@ -3,6 +3,9 @@
   const jugador2 = "O";
   let turno = jugador1;
   let tateti = new Array();
+  tateti[0] = new Array();
+  tateti[1] = new Array();
+  tateti[2] = new Array();
   
   /**
    * Cambia el turno del jugador.
@@ -49,8 +52,59 @@
       cambiarTurno();
     }
   }
-  
+
   const btn00 = document.getElementById("btn00");
   btn00.addEventListener("click", function(evento) {
+    tateti[0,0] = turno;
+    btn00.textContent = turno;
+    cambiarTurno();
+  });
+  const btn01 = document.getElementById("btn01");
+  btn01.addEventListener("click", function(evento) {
+    tateti[0,1] = turno;
+    btn01.textContent = turno;
+    cambiarTurno();
+  });
+  const btn02 = document.getElementById("btn02");
+  btn02.addEventListener("click", function(evento) {
+    tateti[0,2] = turno;
+    btn02.textContent = turno;
+    cambiarTurno();
+  });
+  const btn10 = document.getElementById("btn10");
+  btn10.addEventListener("click", function(evento) {
+    tateti[1,0] = turno;
+    btn10.textContent = turno;
+    cambiarTurno();
+  });
+  const btn11 = document.getElementById("btn11");
+  btn11.addEventListener("click", function(evento) {
+    tateti[1,1] = turno;
+    btn11.textContent = turno;
+    cambiarTurno();
+  });
+  const btn12 = document.getElementById("btn12");
+  btn12.addEventListener("click", function(evento) {
+    tateti[1,2] = turno;
+    btn12.textContent = turno;
+    cambiarTurno();
+  });
+  const btn20 = document.getElementById("btn20");
+  btn20.addEventListener("click", function(evento) {
+    tateti[2,0] = turno;
+    btn20.textContent = turno;
+    cambiarTurno();
+  });
+  const btn21 = document.getElementById("btn21");
+  btn21.addEventListener("click", function(evento) {
+    tateti[2,1] = turno;
+    btn21.textContent = turno;
+    cambiarTurno();
+  });
+  const btn22 = document.getElementById("btn22");
+  btn22.addEventListener("click", function(evento) {
+    tateti[2,2] = turno;
+    btn22.textContent = turno;
+    cambiarTurno();
   });
 })();
